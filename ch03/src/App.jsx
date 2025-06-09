0d94194151f46c4a464a9cc129127e740d9b7ffd
unknown <busan@CC811215017.BNKFGEX.com>
2025-06-09 17:12:24 +0900

    3. 리액트 컴포넌트 실습 init

---
+++ ch03/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*
+  3. 리액트 컴보넌트 실습
+*/
+
+function App() {
+  
+
+  return (
+    <>
+      <h3>3. 리액트 컴포넌트 실습</h3>
+    </>
+  )
+}
+
+export default App
