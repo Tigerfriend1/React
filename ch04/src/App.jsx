bb5df2a1df733b950947a91bdc4f2cc3b278d9b6
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2025-06-10 16:34:13 +0900

    리액트 useState 연습

--- ch04/src/App.jsx
+++ ch04/src/App.jsx
@@ -3,15 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UseRefComponent from './components/UseRefComponent'
+import UseStateComponent from './components/UseStateComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h3>4.리액트 훅</h3>
       <UseRefComponent />
       <UseRefComponent />
+
+      <UseStateComponent />
     </>
   )
 }
