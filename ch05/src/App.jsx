bef75222b39a154510b0035fcfd0be0de9d60cc6
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2025-06-11 15:50:38 +0900

    5. 리액트 라우터 init

---
+++ ch05/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3>5. 리액트 라우터</h3>
+    </>
+  );
+}
+
+export default App;
