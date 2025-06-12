891cc622ec6a9fa7106c3f608c9fd891389f4644
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2025-06-12 16:22:26 +0900

    회원등록, 회원목록조회 구현 HTTP get/post 사용

--- ch07/src/components/UserListComponent.jsx
+++ ch07/src/components/UserListComponent.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function UserListComponent() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    //mount 전
+    fetch(`http://localhost:3000/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //users 상태 업데이트
+        setUsers(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="UserListComponent">
       <h4>회원목록</h4>
@@ -13,16 +29,18 @@ function UserListComponent() {
             <th>나이</th>
             <th>관리</th>
           </tr>
-          <tr>
-            <td>a101</td>
-            <td>왕감자</td>
-            <td>1999-09-09</td>
-            <td>26</td>
-            <td>
-              <button>수정</button>
-              <button>삭제</button>
-            </td>
-          </tr>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.uid}</td>
+              <td>{user.name}</td>
+              <td>{user.birth}</td>
+              <td>{user.age}</td>
+              <td>
+                <button>수정</button>
+                <button>삭제</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
