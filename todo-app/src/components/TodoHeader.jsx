3acdd35c9deb21f6c8849a1101bf822218e7c9de
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2025-06-13 14:40:46 +0900

    to-do 리스트 : 할일추가 기능 구현

---
+++ todo-app/src/components/TodoHeader.jsx
@@ -0,0 +1,12 @@
+import { Plus } from "lucide-react";
+import React from "react";
+
+function TodoHeader() {
+  return (
+    <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+      할 일 목록
+    </h1>
+  );
+}
+
+export default TodoHeader;
---
+++ todo-app/src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import { Check, Edit2, X } from "lucide-react";
+import React from "react";
+import TodoListItem from "./TodoListItem";
+
+function TodoList({ todos }) {
+  return (
+    <div className="space-y-2">
+      <div className="text-center text-gray-500 py-8 select-none">
+        할 일이 없습니다. 새로운 할 일을 추가해보세요!
+      </div>
+
+      {todos.map((todo, index) => (
+        <TodoListItem key={index} todo={todo} />
+      ))}
+      <TodoListItem />
+    </div>
+  );
+}
+
+export default TodoList;
